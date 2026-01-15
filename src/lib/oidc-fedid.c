c91bc8dd3fe21b13d982ba67d3fa92171ac7f145
José Bollo <jose.bollo@iot.bzh>
2026-01-15 18:10:51 +0100

    Refactor of onSocialCheckResult

--- src/lib/oidc-fedid.c
+++ src/lib/oidc-fedid.c
@@ -45,6 +45,145 @@
 #include "oidc-idp-plugin.h"
 #include "oidc-session.h"
 
+/* Send the final status */
+static void fedidEnd(oidcStateT *state, int status, const char *url)
+{
+    EXT_DEBUG("[oidc-fedid] end %d redirect to %s", status, url);
+    if (state->hreq) {
+        char buffer[EXT_URL_MAX_LEN];
+        if (afb_hreq_make_here_url(state->hreq, url, buffer, sizeof(buffer)) >= 0)
+            url = buffer;
+        afb_hreq_redirect_to(state->hreq, url, HREQ_QUERY_EXCL, HREQ_REDIR_TMPY);
+    }
+    if (state->wreq) {
+        struct afb_data *data;
+        afb_data_create_copy(&data, &afb_type_predefined_stringz, url, 1 + strlen(url));
+        afb_req_v4_reply_hookable(state->wreq, status, 1, &data);
+    }
+    oidcStateUnRef(state);
+}
+
+/* Send the final error status */
+static void fedidError(oidcStateT *state)
+{
+    EXT_NOTICE("[oidc-fedid] (hoops!!!) internal error");
+    fedidEnd(state, AFB_ERRNO_INTERNAL_ERROR,
+            oidcCoreGlobals(state->idp->oidc)->errorUrl);
+}
+
+/*
+ * Process a new user
+ * Redirect to the page register or federate to fulfill user data
+ */
+static void fedidNewUser(oidcStateT *state,
+                         oidcSessionT *session,
+                         fedSocialRawT *fedSoc,
+                         fedUserRawT *fedUsr)
+{
+    const char *targetUrl;
+    const oidcProfileT *profile = oidcStateGetProfile(state);
+
+    // fedkey not found let's store social authority profile into session
+    // and redirect user on userprofil creation
+    oidcSessionSetFedUser(session, state->fedUser);
+    oidcSessionSetFedSocial(session, state->fedSocial);
+    if (profile->slave) {
+        oidcSessionSetFedIdLinkRequest(session, FEDID_LINK_REQUESTED);
+        targetUrl = oidcCoreGlobals(state->idp->oidc)->fedlinkUrl;
+    }
+    else {
+        targetUrl = oidcCoreGlobals(state->idp->oidc)->registerUrl;
+    }
+    fedidEnd(state, 0, targetUrl);
+}
+
+// if fedkey exists callback receive local store user profile otherwise we
+// should create it
+static void fedidLoginUser(oidcStateT *state,
+                           oidcSessionT *session,
+                           fedSocialRawT *fedSoc,
+                           fedUserRawT *fedUsr)
+{
+    const oidcProfileT *profile = oidcStateGetProfile(state);
+    const oidcAliasT *alias;
+
+    // let's store user profile into session cookie (/oidc/profile/get
+    // serves it)
+    oidcSessionSetFedUser(session, fedUserAddRef(fedUsr));
+    oidcSessionSetFedSocial(session, state->fedSocial);
+
+    // everything looks good let's return user to original page
+    alias = oidcSessionGetAlias(session);
+
+    // user successfully loggin set session loa to current idp login profile
+    EXT_DEBUG("[oidc-fedid] setting actual profile %s/%s/%d",
+              profile->idp->uid, profile->uid, profile->loa);
+    oidcSessionSetActualLOA(session, profile->loa);
+    oidcSessionSetActualProfile(session, profile);
+    oidcSessionAutoValidate(session);
+
+    fedidEnd(state, 1, alias->url);
+}
+
+static void fedidFederateUser(oidcStateT *state,
+                              oidcSessionT *session,
+                              fedSocialRawT *fedSoc,
+                              fedUserRawT *fedUsr)
+{
+    int err;
+
+    // fedid is already registered
+    fedUsr = fedUserAddRef(fedUsr);
+
+    // check if federation linking is pending
+    const fedSocialRawT *fedSocial;
+    fedSocial = oidcSessionGetFedSocial(session);
+
+
+    // if we have to link two accounts do it before cleaning
+    assert(fedSocial);
+    afb_data_x4_t params[2];
+    int status;
+    unsigned int count;
+    afb_data_t data;
+
+    // make sure we do not link account twice
+    oidcSessionSetFedIdLinkRequest(session, FEDID_LINK_RESET);
+
+    // delegate account federation linking to fedid binding
+    err = afb_create_data_raw(&params[0], fedUserObjType, fedUsr, 0,
+                              NULL, NULL);
+    if (err < 0)
+        return fedidError(state);
+    err = afb_create_data_raw(&params[1], fedSocialObjType, fedSocial,
+                              0, NULL, NULL);
+    if (err < 0)
+        return fedidError(state);
+    afb_api_t api = oidcCoreAfbApi(state->idp->oidc);
+    err = fedIdClientCallSync(api, "user-federate", 2, params, &status,
+                              &count, &data);
+    if (err < 0 || status != 0) {
+        EXT_ERROR(
+            "[oidc-fedid] fail to link account pseudo=%s "
+            "email=%s",
+            fedUsr->pseudo, fedUsr->email);
+        return fedidError(state);
+    }
+    return fedidLoginUser(state, session, fedSoc, fedUsr);
+}
+
+static void fedidExistingUser(oidcStateT *state,
+                              oidcSessionT *session,
+                              fedSocialRawT *fedSoc,
+                              fedUserRawT *fedUsr)
+{
+    int fedLoa = oidcSessionGetFedIdLinkRequest(session);
+    if (fedLoa == FEDID_LINK_REQUESTED)
+        fedidFederateUser(state, session, fedSoc, fedUsr);
+    else
+        fedidLoginUser(state, session, fedSoc, fedUsr);
+}
+
 // if fedkey exists callback receive local store user profile otherwise we
 // should create it
 static void onSocialCheckResult(void *closure,
@@ -53,164 +192,25 @@ static void onSocialCheckResult(void *closure,
                                 fedUserRawT *fedUsr)
 {
     oidcStateT *state = (oidcStateT *)closure;
-    char url[EXT_URL_MAX_LEN];
-    const char *target;
-    fedUserRawT *fedUser;
-    const oidcProfileT *profile = oidcStateGetProfile(state);
-    const oidcAliasT *alias;
     oidcSessionT *session = oidcStateGetSession(state);
-    const char *redirect;
-    struct afb_hreq *hreq = state->hreq;
-    struct afb_req_v4 *wreq = state->wreq;
-    int err;
+    int sessionLoa = oidcSessionGetActualLOA(session);
 
     EXT_WARNING("*** entering Social check status=%d ***", status);
 
     // internal API error
     if (status < 0) {
         EXT_WARNING("Social check got error %d", status);
-        goto OnErrorExit;
+        return fedidError(state);
     }
 
     // user try to login if loa set then reset session
-    int sessionLoa = oidcSessionGetActualLOA(session);
     if (sessionLoa)
         fedidsessionReset(session, NULL);
 
-    if (status != 1) {  // fedid is not registered and we are not facing a
-        // secondary authentication
-        const char *targetUrl;
-
-        // fedkey not found let's store social authority profile into session
-        // and redirect user on userprofil creation
-        oidcSessionSetFedUser(session, state->fedUser);
-        oidcSessionSetFedSocial(session, state->fedSocial);
-        if (profile->slave) {
-            oidcSessionSetFedIdLinkRequest(session, FEDID_LINK_REQUESTED);
-            targetUrl = oidcCoreGlobals(state->idp->oidc)->fedlinkUrl;
-        }
-        else {
-            targetUrl = oidcCoreGlobals(state->idp->oidc)->registerUrl;
-        }
-        if (hreq) {
-            const char *params[] = {
-#if FORCELANG
-                "language", setlocale(LC_CTYPE, ""),
-#endif
-                NULL};
-            size_t sz =
-                rp_escape_url_to(NULL, targetUrl, params, url, sizeof url);
-            if (sz >= sizeof url) {
-                EXT_ERROR(
-                    "[fedid-register-unknown] fail to build redirect url");
-                goto OnErrorExit;
-            }
-        }
-        else {
-            target = targetUrl;
-        }
-    }
-    else {  // fedid is already registered
-        fedUser = fedUserAddRef(fedUsr);
-
-        // check if federation linking is pending
-        const fedSocialRawT *fedSocial;
-        fedSocial = oidcSessionGetFedSocial(session);
-        int fedLoa = oidcSessionGetFedIdLinkRequest(session);
-
-        // if we have to link two accounts do it before cleaning
-        if (fedLoa == FEDID_LINK_REQUESTED) {
-            assert(fedSocial);
-            afb_data_x4_t params[2];
-            int status;
-            unsigned int count;
-            afb_data_t data;
-
-            // make sure we do not link account twice
-            oidcSessionSetFedIdLinkRequest(session, FEDID_LINK_RESET);
-
-            // delegate account federation linking to fedid binding
-            err = afb_create_data_raw(&params[0], fedUserObjType, fedUser, 0,
-                                      NULL, NULL);
-            if (err < 0)
-                goto OnErrorExit;
-            err = afb_create_data_raw(&params[1], fedSocialObjType, fedSocial,
-                                      0, NULL, NULL);
-            if (err < 0)
-                goto OnErrorExit;
-            afb_api_t api = oidcCoreAfbApi(state->idp->oidc);
-            err = fedIdClientCallSync(api, "user-federate", 2, params, &status,
-                                      &count, &data);
-            if (err < 0 || status != 0) {
-                EXT_ERROR(
-                    "[fedid-link-account] fail to link account pseudo=%s "
-                    "email=%s",
-                    fedUser->pseudo, fedUser->email);
-                goto OnErrorExit;
-            }
-        }
-        // let's store user profile into session cookie (/oidc/profile/get
-        // serves it)
-        oidcSessionSetFedUser(session, fedUserAddRef(fedUser));
-        oidcSessionSetFedSocial(session, state->fedSocial);
-
-        // everything looks good let's return user to original page
-        alias = oidcSessionGetAlias(session);
-
-        size_t sz = rp_escape_url_to(NULL, alias->url, NULL, url, sizeof url);
-        if (sz >= sizeof url) {
-            EXT_ERROR("[oidc-fedid] fail to build redirect url");
-            goto OnErrorExit;
-        }
-
-        if (hreq) {
-            // add afb-binder endpoint to login redirect alias
-            err = afb_hreq_make_here_url(hreq, alias->url, url, sizeof(url));
-            if (err < 0) {
-                EXT_ERROR("[oidc-fedid] fail to build redirect url");
-                goto OnErrorExit;
-            }
-        }
-        else {
-            target = alias->url;
-        }
-
-        // user successfully loggin set session loa to current idp login profile
-        EXT_DEBUG("[oidc-fedid] setting actual profile %s/%s/%d",
-                  profile->idp->uid, profile->uid, profile->loa);
-        oidcSessionSetActualLOA(session, profile->loa);
-        oidcSessionSetActualProfile(session, profile);
-        oidcSessionAutoValidate(session);
-    }
-
-    // free user info handle and redirect to initial targeted url
-    if (hreq) {
-        EXT_DEBUG("[fedid-check-redirect] redirect to %s", url);
-        afb_hreq_redirect_to(hreq, url, HREQ_QUERY_EXCL, HREQ_REDIR_TMPY);
-    }
-    else {
-        struct afb_data *reply;
-        json_object *responseJ;
-
-        rp_jsonc_pack(&responseJ, "{ss}", "target", target);
-
-        EXT_DEBUG("[fedid-check-reply] {'target':'%s'}", target);
-        afb_data_create_raw(&reply, &afb_type_predefined_json_c, responseJ, 0,
-                            (void *)json_object_put, responseJ);
-        afb_req_v4_reply_hookable(wreq, status, 1, &reply);
-    }
-
-    oidcStateUnRef(state);
-    return;
-
-OnErrorExit:
-    EXT_NOTICE("[fedid-authent-redirect] (hoops!!!) internal error");
-    if (hreq)
-        afb_hreq_redirect_to(hreq, oidcCoreGlobals(state->idp->oidc)->errorUrl,
-                             HREQ_QUERY_EXCL, HREQ_REDIR_TMPY);
-    if (wreq)
-        afb_req_v4_reply_hookable(wreq, -1, 0, NULL);
-    oidcStateUnRef(state);
+    if (status == 0)
+        fedidNewUser(state, session, fedSoc, fedUsr);
+    else
+        fedidExistingUser(state, session, fedSoc, fedUsr);
 }
 
 // try to wreq user profile from its federation key
@@ -220,3 +220,4 @@ int fedidCheck(oidcStateT *state)
     fedIdClientSocialCheck(api, state->fedSocial, onSocialCheckResult, state);
     return 0;
 }
+
